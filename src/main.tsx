88c9f83630bbf49cd19ebdcbc605546b9bb45aec
Neon <tpshine1234@gmail.com>
2024-10-20 15:00:39 -0700

    Development

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDom from 'react-dom/client';
+import {BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App';
+
+ReactDom.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
